993d118b89fd46a57f1612a2c9485a344d93a3db
atlee19 <atleegraham14@gmail.com>
2019-11-24 16:56:52 -0500

    prototype for binToDec added

---
+++ binToDec_test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "myStack.h" //array-stack code from lab13
+
+using namespace std;
+
+void binToDec(int binaryNumber, int &decimal, int &weight);
+/*
+    This function takes a binary number of any size and converts
+    it to a decimal number.
+    Precondtions:
+        - decimal is set to 0
+        - weight is set to 0  
+*/
+
+
+int main()
+{
+
+
+
+    return 0;
+}
