9f9c32d388ef6700681cdd02c4c122a6f8f12098
atlee19 <atleegraham14@gmail.com>
2019-11-25 13:42:05 -0500

    added prototype for decToBin

--- decToBin_test.cpp
+++ decToBin_test.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+void decToBin(int number, int base);
+/*
+    This function takes a positive integer and converts
+    it to its binary form in base 2. 
+    Precondtions:
+        number = > 0
+        base == 2 
+*/
+
 int main()
 {
 
